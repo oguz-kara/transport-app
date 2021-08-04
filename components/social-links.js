e64c3ff1f743c5d28eec7d61f4cbc791449bc540
oguz-kara <dev.hasan.kara@gmail.com>
2021-08-04 23:15:38 +0300

    git statusparasite... only little change to social-links components it was against the name of branch but I had to add

--- components/social-links.js
+++ components/social-links.js
@@ -1,10 +1,17 @@
 import Link from "next/link";
 import styles from "../styles/social-links.module.scss";
 import { Twitter, Facebook, Instagram, Pinterest } from "@material-ui/icons";
-export default function SocialLinks({ color, fontSize }) {
+export default function SocialLinks({ color, fontSize, show }) {
+  const getStyle = (show = false) => {
+    if (show) {
+      return styles.socialLinks2;
+    } else {
+      return styles.socialLinks;
+    }
+  };
   return (
     <div
-      className={styles.socialLinks}
+      className={getStyle(show)}
       style={{ color: color, fontSize: fontSize }}
     >
       <Link className={styles.socialLink} href="/" passHref>
