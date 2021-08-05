0cdca9be5327f4123553fb948ab2bef11b26f330
oguz-kara <dev.hasan.kara@gmail.com>
2021-08-06 00:15:19 +0300

    responsive navbar finished.

--- components/navbar.js
+++ components/navbar.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import ListLink from "./list-link";
 import styles from "../styles/navbar.module.scss";
 import Link from "next/link";
 import Image from "next/image";
@@ -9,138 +9,54 @@ export function MbNavbar({ active, onClick }) {
     <div className={`${styles.mbNav} ${active && styles.active}`}>
       <div className={styles.exitContainer}>
         <button onClick={(e) => onClick(e)}>
-          <Close style={{ fontSize: 50, margin: 5 }} />
+          <Close style={{ fontSize: 50, margin: "15px 30px 15px 0" }} />
         </button>
       </div>
       <ul>
-        <li className={styles.listItem}>
-          <Link href="/" passHref>
-            <a className={styles.navbarLink}>
-              <span>home</span>
-              <span>
-                <ChevronRight className={styles.chevronRightIcon} />
-              </span>
-            </a>
-          </Link>
-          <ul className={styles.collapseMenu}>
-            <li>
-              <Link href="/" passHref>
-                Some link
-              </Link>
-            </li>
-            <li>
-              <Link href="/" passHref>
-                Some link 2
-              </Link>
-            </li>
-            <li>
-              <Link href="/" passHref>
-                Some link
-              </Link>
-            </li>
-          </ul>
-        </li>
-        <li className={styles.listItem}>
-          <Link href="/" passHref>
-            get taxi
-          </Link>
-        </li>
-        <li className={styles.listItem}>
-          <Link href="/" passHref>
-            services
-          </Link>
-        </li>
-        <li className={styles.listItem}>
-          <Link href="/" passHref>
-            <a className={styles.navbarLink}>
-              <span>our blog</span>
-              <span>
-                <ChevronRight className={styles.chevronRightIcon} />
-              </span>
-            </a>
-          </Link>
-          <ul className={styles.collapseMenu}>
-            <li>
-              <Link href="/" passHref>
-                Some link
-              </Link>
-            </li>
-            <li>
-              <Link href="/" passHref>
-                Some link 2
-              </Link>
-            </li>
-            <li>
-              <Link href="/" passHref>
-                Some link
-              </Link>
-            </li>
-          </ul>
-        </li>
-        <li className={styles.listItem}>
-          <Link href="/" passHref>
-            <a className={styles.navbarLink}>
-              <span>gallery</span>
-              <span>
-                <ChevronRight className={styles.chevronRightIcon} />
-              </span>
-            </a>
-          </Link>
-          <ul className={styles.collapseMenu}>
-            <li>
-              <Link href="/" passHref>
-                Some link
-              </Link>
-            </li>
-            <li>
-              <Link href="/" passHref>
-                Some link 2
-              </Link>
-            </li>
-            <li>
-              <Link href="/" passHref>
-                Some link
-              </Link>
-            </li>
-          </ul>
-        </li>
-        <li className={styles.listItem}>
-          <Link href="/" passHref>
-            testimonials
-          </Link>
-        </li>
-        <li className={styles.listItem}>
-          <Link href="/" passHref>
-            contact
-          </Link>
-        </li>
-        <li className={styles.listItem}>
-          <Link href="/" passHref>
-            <a className={styles.navbarLink}>
-              <span>pages</span>
-              <span>
-                <ChevronRight className={styles.chevronRightIcon} />
-              </span>
-            </a>
-          </Link>
-          <ul className={styles.collapseMenu}>
-            <li>
-              <Link href="/" passHref>
-                Some link
-              </Link>
-            </li>
-            <li>
-              <Link href="/" passHref>
-                Some link 2
-              </Link>
-            </li>
-            <li>
-              <Link href="/" passHref>
-                Some link
-              </Link>
-            </li>
-          </ul>
-        </li>
+        <ListLink
+          collapseable
+          items={[
+            { href: "/", name: "some link" },
+            { href: "/", name: "some link" },
+            { href: "/", name: "some link" },
+          ]}
+        >
+          home
+        </ListLink>
+        <ListLink>get taxi</ListLink>
+        <ListLink>services</ListLink>
+        <ListLink
+          collapseable
+          items={[
+            { href: "/", name: "some link" },
+            { href: "/", name: "some link" },
+            { href: "/", name: "some link" },
+          ]}
+        >
+          our blog
+        </ListLink>
+        <ListLink
+          collapseable
+          items={[
+            { href: "/", name: "some link" },
+            { href: "/", name: "some link" },
+            { href: "/", name: "some link" },
+          ]}
+        >
+          gallery
+        </ListLink>
+        <ListLink>testimonials</ListLink>
+        <ListLink>contact</ListLink>
+        <ListLink
+          collapseable
+          items={[
+            { href: "/", name: "some link" },
+            { href: "/", name: "some link" },
+            { href: "/", name: "some link" },
+          ]}
+        >
+          pages
+        </ListLink>
       </ul>
     </div>
   );
